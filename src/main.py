d3c8dd8a31df26dfb6d7a9de86e2765c6b8ee366
Mateusz Andruszkiewicz <mateusz.andruszkiewicz@bitfold.com>
2025-12-10 17:14:43 +0000

    Wszystko ok do generowania instrukcji.

--- src/main.py
+++ src/main.py
@@ -1,115 +1,13 @@
 #import RPi.GPIO as GPIO
+import enum
 from time import sleep
-from PIL import Image
-import numpy as np
 import json
 import argparse
 import pdb
-
-parser = argparse.ArgumentParser(description="Opis")
-parser.add_argument("input", type=str, help="Obraz")
-
-args = parser.parse_args()
-
-print("Użyty obraz: ", args.input)
-
-# brakuje 2 rzeczy: 
-# 1 - usuwanie zbędnych zer jeżeli w następnej linijce jest mniej pixeli i głowiaca zamiast dojechać do końca i wrócić, powinna od razu zejść do następnej lini.
-# 2 - dodawanie zer jeżeli następna linia jest dłuższa niż poprzednia.
-
-print(' N = pusta linia')
-print(' P = głowica jedzie w prawo')
-print(' L = głowica jedzie w lewo')
-print(' K = koniec pliku')
+from module.instruction import make_instruction as make
 
 
-def picture_to_array(image):
-    binary_array = []
-    pixels_quantity = 0
-    lines_quantity = 0
-    arr = np.array(image)
-
-    for line in arr:
-        lines_quantity+=1
-        pixels_quantity_in_line = 0
-        linia = [] #lista do tymczasowego zapisu dla pixeli w lini
-    
-        for pixel in line:
-            pixels_quantity+=1
-            pixels_quantity_in_line+=1
-            suma = int(pixel[0]) + int(pixel[1]) + int(pixel[2])
-            if suma > 500:
-                linia.append(0)
-            else:
-                linia.append(1)
-        binary_array.append(linia)
-    print('ilość lini = ', len(binary_array))
-
-    return binary_array
-
-def print_2D_array(array):
-    for line in array:
-        for item in line:
-            if item == 0:
-                print('•', end = '')
-            else:
-                print('■', end = '')
-        print('')
-
-def prepare_instruction(binary_array):
-    marks_quantity = 0
-    instruction = []
-
-    kierunek = False # w lewo to ujemny czyli *False*, a w prawo to dodatni, czyli *True*
-    #pdb.set_trace()
-    for i, line in enumerate(binary_array): # sprawdzamy czy całą linia jest pusta. Jeżeli tak to oznaczamy jako 'N' = Next, Następna
-        try:
-            suma_lini = sum(line) 
-            if suma_lini == 0:
-                instruction.append("N")
-            else:
-                if kierunek == True:
-                    instruction.append("R")
-                elif kierunek == False:
-                    instruction.append("L")
-                    line.reverse()
-                for mark in line:
-                    instruction.append(mark)
-                kierunek = not kierunek
-        except:
-            print("EXCEPTION!!!")
-    
-    try: 
-       instruction.pop() # Usuwamy zbedne dane z końca
-    except IndexError as e:
-        pass
-    instruction.append("K") # znak końca pliku
-    return instruction
-
-def remove_unnecessary_pixels(instruction):
-    flag = True
-    while flag:
-        quantity = 0
-        indices = [i for i, item in enumerate(instruction) if item in ('R', 'L')]
-        for index in indices:
-            if instruction[index-1] == 0 and instruction[index+1] == 0:
-                instruction.pop(index-1)
-                instruction.pop(index)
-                quantity = quantity + 1
-        if quantity == 0:
-            flag = False 
-    print(instruction)
-        # Oznaczamy zakończenie pliku
-
-# ---- Tworzenie obrazu ----
-
-img = Image.open(args.input).convert("RGB")
-binary_2D_image = picture_to_array(img)
-print_2D_array(binary_2D_image)
-instruction = prepare_instruction(binary_2D_image)
-remove_unnecessary_pixels(instruction)
-
-# ---- 3. Rozpoczęcie druk ----
+# ======  ASSERTION  ======
 
 # Ustawienia GPIO
 #
@@ -133,6 +31,43 @@ remove_unnecessary_pixels(instruction)
 #GPIO.output(DIR, GPIO.HIGH)
 #GPIO.output(ENA, GPIO.LOW)
 #
+
+
+
+# =========  ACT  =========
+
+instruction = []
+try:
+    parser = argparse.ArgumentParser(description="Opis")
+    parser.add_argument("input", type=str, help="Obraz")
+    args = parser.parse_args()
+    image_path = args.input
+    print("Użyty obraz: ",image_path)
+    instruction = make(image_path)
+except Exception as e:
+    print('SOMETHING IS NO YES')
+    print(e)
+
+
+for mark in instruction:
+    match mark:
+        case 'N':
+            print('Nowa linia')
+        case 'L':
+            print('W lewo')
+        case 'R':
+            print('W prawo')
+        case 'K':
+            print('Koniec!')
+        case 0:
+            print('Wyłącz laser, Szybko omiń')
+        case 1:
+            print('Utrzymaj laser, przytrzymaj króciutko, po czym zrób krok')
+        case 2:
+            print('Włącz LASER i przytrzymaj chwilę dłużej, po czym zrób krok')
+        case _:
+            print(f"Nie ma takiego znaku: \"{mark}\"")
+
 #for j in range(10):
 #    for i in range(200):
 #        print(i)
---
+++ src/module/instruction.py
@@ -0,0 +1,144 @@
+from PIL import Image
+import numpy as np
+
+def make_instruction(path2image):
+    img = Image.open(path2image).convert("RGB")
+    binary_2D_image = picture_to_array(img)
+    print_2D_array(binary_2D_image)
+    instruction = prepare_instruction(binary_2D_image)
+    print(' P = pusta linia')
+    print(' N = nowa linia')
+    print(' R = głowica jedzie w prawo')
+    print(' L = głowica jedzie w lewo')
+    print(' K = koniec pliku')
+    print(instruction)
+    return instruction
+
+def picture_to_array(image):
+    binary_array = []
+    pixels_quantity = 0
+    lines_quantity = 0
+    arr = np.array(image)
+    for line in arr:
+        lines_quantity+=1
+        pixels_quantity_in_line = 0
+        linia = [] #lista do tymczasowego zapisu dla pixeli w lini
+        for pixel in line:
+            pixels_quantity+=1
+            pixels_quantity_in_line+=1
+            suma = int(pixel[0]) + int(pixel[1]) + int(pixel[2])
+            if suma > 500:
+                linia.append(0)
+            else:
+                linia.append(1)
+        binary_array.append(linia)
+    print('ilość lini = ', len(binary_array))
+    return binary_array
+
+def print_2D_array(array):
+    for line in array:
+        for item in line:
+            if item == 0:
+                print('•', end = '')
+            else:
+                print('■', end = '')
+        print('')
+
+def prepare_instruction(binary_array):
+    instruction = set_dir(binary_array)
+    instruction.append("K") # znak końca pliku
+    instruction = remove_unnecessary_pixels(instruction)
+    instruction = add_next_line_mark(instruction)
+    instruction = remove_0_between_empty_lines(instruction)
+    while instruction[-2] == 0 or instruction[-2] == 'N':
+        instruction.pop(-2)
+    for i, mark in enumerate(instruction):
+        if i == 0 or mark == 'K':
+            pass
+        else:
+            if instruction[i-1] != 1  and instruction[i] == 1:
+                if instruction[i-1] == 2:
+                    pass
+                else:
+                    instruction[i] = 2
+    return instruction
+
+
+def set_dir(binary_array):
+    instruction = []
+    kierunek = False # w lewo to ujemny czyli *False*, a w prawo to dodatni, czyli *True*
+    for line in binary_array: # sprawdzamy czy całą linia jest pusta. Jeżeli tak to oznaczamy jako 'N' = Następna
+        try:
+            suma_lini = sum(line) 
+            if suma_lini == 0:
+                instruction.append("N")
+            else:
+                if kierunek == True:
+                    instruction.append("R")
+                elif kierunek == False:
+                    instruction.append("L")
+                    line.reverse()
+                for mark in line:
+                    instruction.append(mark)
+                kierunek = not kierunek
+        except:
+            print("EXCEPTION!!!")
+    return instruction
+
+def remove_0_between_empty_lines(instruction):
+    NN = 1
+    while NN > 0:
+        NN = 0
+        N_after_N_list = find_multiple_n_starts(instruction)
+        for i in N_after_N_list:
+            if instruction[i[0]-1] == 0 and instruction[i[0]+i[1]+1] == 0:
+                instruction.pop(i[0]+i[1]+1)
+                instruction.pop(i[0]-1)
+                NN = NN + 1
+    return instruction
+
+def remove_unnecessary_pixels(instruction):
+    flag = True
+    while flag:
+        quantity = 0
+        indices = [i for i, item in enumerate(instruction) if item in ('R', 'L')]
+        indices.reverse()
+        for index in indices:
+            if instruction[index-1] == 0 and instruction[index+1] == 0:
+                instruction.pop(index+1)
+                instruction.pop(index-1)
+                quantity = quantity + 1
+        if quantity == 0:
+            flag = False
+    return instruction
+    
+def add_next_line_mark(instruction):
+    indices = [i for i, item in enumerate(instruction) if item in ('R', 'L')]
+    indices.reverse()
+    for index in indices:
+        instruction.insert(index, 'N')
+    if instruction[0] == 'N' and instruction[1] == 'N':
+        instruction.pop(0)
+    return instruction
+
+def find_multiple_n_starts(seq):
+    indices_n_start = []
+    i = 0
+    while i < len(seq):
+        # Sprawdzamy czy to litera 'N'
+        if isinstance(seq[i], str) and seq[i] == 'N':
+            # Zliczamy wszystkie kolejne N
+            n_count = 1
+            j = i + 1
+            while j < len(seq) and isinstance(seq[j], str) and seq[j] == 'N':
+                n_count += 1
+                j += 1
+            # Jeśli 2 lub więcej N, dodajemy do wyniku
+            if n_count >= 2:
+                indices_n_start.append((i, n_count))
+            # Przeskakujemy całą sekwencję N
+            i = j
+        else:
+            i += 1
+    
+    return indices_n_start
